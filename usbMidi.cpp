31f0f721de755009a4fe2b08ed526a1cb035a88a
lanmower <almagestfraternite@gmail.com>
2026-04-15 19:59:47 +0200

    fix: remove constant MIDI device scanning, gate to USB PnP events only
    Constant scanning every 30 iterations (8db54da) was generating syslog spam
    at 1/frame rate. Device enumeration should only occur on USB plug-and-play
    events. Restores early return gate that was removed in 8db54da.

--- usbMidi.cpp
+++ usbMidi.cpp
@@ -20,38 +20,20 @@ static int s_updateCount = 0;
 
 void usbMidiProcess(bool bPlugAndPlayUpdated)
 {
-    s_updateCount++;
-    bool bNeedsScan = bPlugAndPlayUpdated || (s_updateCount % 30 == 1);
+    if (!bPlugAndPlayUpdated) return;
 
-    // Every 30th iteration, log which umidi slots are present
-    if (bNeedsScan && s_updateCount % 30 == 1)
+    CString name;
+    for (int i = 1; i <= 8; i++)
     {
-        CString name;
-        CString found;
-        for (int i = 1; i <= 4; i++)
-        {
-            name.Format("umidi%d", i);
-            void *p = CDeviceNameService::Get()->GetDevice((const char *)name, FALSE);
-            if (p) { found.Append((const char *)name); found.Append(" "); }
-        }
-        CLogger::Get()->Write(log_name, LogNotice, "PnP#%d umidi scan: [%s]",
-            s_updateCount, found.GetLength() ? (const char *)found : "none");
-    }
-
-    if (bNeedsScan) {
-        CString name;
-        for (int i = 1; i <= 8; i++)
-        {
-            if (s_registered[i]) continue;
-            name.Format("umidi%d", i);
-            CUSBMIDIDevice *pDev = (CUSBMIDIDevice *)
-                CDeviceNameService::Get()->GetDevice((const char *)name, FALSE);
-            if (!pDev) continue;
-            s_pDevices[i] = pDev;
-            s_registered[i] = true;
-            CLogger::Get()->Write(log_name, LogNotice, "USB MIDI device connected: %s", (const char *)name);
-            pDev->RegisterPacketHandler(packetHandler);
-        }
+        if (s_registered[i]) continue;
+        name.Format("umidi%d", i);
+        CUSBMIDIDevice *pDev = (CUSBMIDIDevice *)
+            CDeviceNameService::Get()->GetDevice((const char *)name, FALSE);
+        if (!pDev) continue;
+        s_pDevices[i] = pDev;
+        s_registered[i] = true;
+        CLogger::Get()->Write(log_name, LogNotice, "USB MIDI device connected: %s", (const char *)name);
+        pDev->RegisterPacketHandler(packetHandler);
     }
 }
 
